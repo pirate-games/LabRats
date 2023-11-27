73df2779021624f619e56a6e9be1e70d07a7385b
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2023-11-27 14:31:57 +0100

    Added relay and cross pc mulityplayer fuck

--- Lab Rats_clone_0/Assets/Mulitplayer/NetworkUI/NetworkConnecter.cs
+++ Lab Rats_clone_0/Assets/Mulitplayer/NetworkUI/NetworkConnecter.cs
@@ -1,17 +1,73 @@
+using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Authentication;
 using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
 
 namespace Mulitplayer.NetworkUI
 {
     public class NetworkConnecter : MonoBehaviour
     {
-        public void Create()
+
+        private Lobby _currentLobby;
+
+        [SerializeField] private int maximumConnections = 2;
+        [SerializeField] private UnityTransport transport;
+
+        private async void Awake()
         {
+            await UnityServices.InitializeAsync();
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        public async void Create()
+        {
+            var allocation = await RelayService.Instance.CreateAllocationAsync(maximumConnections);
+            var generatedJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            // Set the host data for the transport of the NetworkManager
+            transport.SetHostRelayData(
+                allocation.RelayServer.IpV4,
+                (ushort) allocation.RelayServer.Port,
+                allocation.AllocationIdBytes,
+                allocation.Key,
+                allocation.ConnectionData
+            );
+
+            var lobbyOptions = new CreateLobbyOptions()
+            {
+                IsPrivate = false,
+                Data = new Dictionary<string, DataObject>()
+            };
+
+            var dataObject = new DataObject(DataObject.VisibilityOptions.Public, generatedJoinCode);
+            lobbyOptions.Data.Add("JoinCode", dataObject);
+
+            _currentLobby = await LobbyService.Instance.CreateLobbyAsync("TestLobby", maximumConnections);
+
             NetworkManager.Singleton.StartHost();
         }
 
-        public void Join()
+        public async void Join()
         {
+
+            _currentLobby = await LobbyService.Instance.QuickJoinLobbyAsync();
+            
+            var joinCode = _currentLobby.Data["JoinCode"].Value;
+            var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            transport.SetHostRelayData(
+                joinAllocation.RelayServer.IpV4,
+                (ushort) joinAllocation.RelayServer.Port,
+                joinAllocation.AllocationIdBytes,
+                joinAllocation.Key,
+                joinAllocation.HostConnectionData
+            );
+
             NetworkManager.Singleton.StartClient();
         }
     }
