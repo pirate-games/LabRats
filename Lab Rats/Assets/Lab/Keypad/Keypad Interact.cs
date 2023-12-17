10dd6ce1dee480349f6285f69e93e702cbf160ba
Tiara Vrossink <71695366+arctantica@users.noreply.github.com>
2023-12-17 14:07:36 +0100

    Keypad ready for testing, Asset imported not implemented

--- Lab Rats/Assets/Lab/Keypad/Keypad Interact.cs
+++ Lab Rats/Assets/Lab/Keypad/Keypad Interact.cs
@@ -7,6 +7,8 @@ public class KeypadInteract : MonoBehaviour
     [SerializeField]
     private KeypadFunctionality keypad;
     [SerializeField]
+    private GameObject popupText;
+    [SerializeField]
     private int length;
     [SerializeField]
     private string code;
@@ -22,18 +24,42 @@ public class KeypadInteract : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+/*    private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "MainCamera")
         {
-            Debug.Log("test");
+            popupText.SetActive(true);
             if (Input.GetKeyDown(KeyCode.E))
             {
+                popupText.SetActive(false);
                 keypad.gameObject.SetActive(true);
                 keypad.codeLength = length;
                 keypad.correctCode = code;
                 keypad.clearAll();
             }
+        }
+    }*/
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "MainCamera")
+        {
+            popupText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                popupText.SetActive(false);
+                keypad.gameObject.SetActive(true);
+                keypad.codeLength = length;
+                keypad.correctCode = code;
+                keypad.clearAll();
+            }
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if(collision.gameObject.tag == "MainCamera")
+        {
+            popupText.SetActive(false);
         }
     }
 }
