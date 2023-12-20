0e18e9c2ea37b5d1351926cdde3b9c01191b3490
YoloJordy <jordypost@live.nl>
2023-12-20 12:40:23 +0100

    Added the ability to changed gradients from the script

--- Lab Rats/Assets/Lab/Bunson Puzzle/Prefabs/Flame Controller.cs
+++ Lab Rats/Assets/Lab/Bunson Puzzle/Prefabs/Flame Controller.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FlameController : MonoBehaviour
@@ -5,6 +7,7 @@ public class FlameController : MonoBehaviour
     ParticleSystem particles;
     ParticleSystem.MainModule main;
     ParticleSystem.VelocityOverLifetimeModule velocity;
+    ParticleSystem.ColorOverLifetimeModule color;
 
     private float startParticleSize;
     private float velocityChangeY;
@@ -12,12 +15,87 @@ public class FlameController : MonoBehaviour
     [SerializeField]
     private float minFlameSize;
 
+    [Header ("Gradient Presets")]
+    [SerializeField]
+    Gradient defaultGradient;
+    [SerializeField]
+    Gradient redGradient;
+    [SerializeField]
+    Gradient orangeGradient;
+    [SerializeField]
+    Gradient pinkGradient;
+    [SerializeField]
+    Gradient purpleGradient;
+    [SerializeField]
+    Gradient greenGradient;
+    [SerializeField]
+    Gradient whiteBlueGradient;
+    [SerializeField]
+    Gradient whiteGradient;
+
+    [Header("Colored Objects")]
+    [SerializeField]
+    FlameElement[] flameElements;
+    Dictionary<string, FlameElement> flameDict = new();
+
+    public enum FlameColor
+    {
+        Red,
+        Orange,
+        Pink,
+        Purple,
+        Green,
+        WhiteBlue,
+        White,
+        Default
+    }
+
+    [Serializable]
+    public struct FlameElement
+    {
+        public string name;
+        public FlameColor color;
+    }
+
+    private FlameColor _flameColor = FlameColor.Default;
+
+    private FlameColor CurrentFlameColor
+    {
+        get { return _flameColor; }
+        set 
+        { 
+            if (particles == null) return;
+
+            _flameColor = value;
+            color.color = value switch
+            {
+                FlameColor.Red => redGradient,
+                FlameColor.Orange => orangeGradient,
+                FlameColor.Pink => pinkGradient,
+                FlameColor.Purple => purpleGradient,
+                FlameColor.Green => greenGradient,
+                FlameColor.WhiteBlue => whiteBlueGradient,
+                FlameColor.White => whiteGradient,
+                _ => defaultGradient
+            };
+        }
+    }
+
+    private List<GameObject> collidingObjects = new();
+
     private void Start()
     {
+        //initialise the dict with the elements from the inspector
+        foreach (var flameElement in flameElements)
+        {
+            flameDict[flameElement.name] = flameElement;
+        }
+
         if (!TryGetComponent(out particles)) return;
 
         main = particles.main;
         velocity = particles.velocityOverLifetime;
+        color = particles.colorOverLifetime;
 
         startParticleSize = main.startSizeMultiplier;
         velocityChangeY = velocity.yMultiplier;
@@ -26,6 +104,10 @@ public class FlameController : MonoBehaviour
         velocity.yMultiplier = 0;
     }
 
+    /// <summary>
+    ///   Adjusts the size multiplier of the flame
+    /// </summary>
+    /// <param name="size"> Is multiplied by the original values of main.startSizeMultiplier and velocity.yMultiplier from the inspector </param>
     public void SetFlameSize(float size)
     {
         if (particles == null) return;
@@ -34,5 +116,40 @@ public class FlameController : MonoBehaviour
 
         main.startSizeMultiplier = startParticleSize * size;
         velocity.yMultiplier = velocityChangeY * size;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (particles == null) return;
+
+        //adds new collision to list
+        if (!collidingObjects.Contains(other.gameObject))
+        {
+            collidingObjects.Add(other.gameObject);
+        }
+
+        //changes color of flame based on newest collision
+        if (flameDict.TryGetValue(other.name, out var flame))
+            CurrentFlameColor = flame.color;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (particles == null) return;
+
+        //removes object from collisions list
+        if (collidingObjects.Contains(other.gameObject))
+        {
+            collidingObjects.Remove(other.gameObject);
+        }
+
+        //checks wether the color needs to be adjusted
+        if (flameDict.TryGetValue(other.name, out var oldFlame) && oldFlame.color == CurrentFlameColor)
+        {
+            if (collidingObjects.Count > 0 && flameDict.TryGetValue(collidingObjects[0].name, out var flame))
+                CurrentFlameColor = flame.color;
+            else
+                CurrentFlameColor = FlameColor.Default;
+        }
     }
 }
