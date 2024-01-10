f5ed229357c7e2c3d54c5bd174bc549df8f37f6f
Tiara Vrossink <71695366+arctantica@users.noreply.github.com>
2024-01-10 14:39:12 +0100

    oven fixed?

--- Lab Rats/Assets/Lab/Oven/Scripts/OvenFunctionality.cs
+++ Lab Rats/Assets/Lab/Oven/Scripts/OvenFunctionality.cs
@@ -15,6 +15,8 @@ public class OvenFunctionality : MonoBehaviour
 
     private float lightIntense = 0.4f, doorClosed = 88f;
 
+    private bool isActive;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,19 +26,22 @@ public class OvenFunctionality : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(door.transform.rotation.eulerAngles.y);
-
+        if (isActive)
+        {
+            if (collider.steelCount >= 2 && collider.mouldInside && door.transform.rotation.eulerAngles.y >= doorClosed)
+            {
+                collider.steel1.SetActive(false);
+                collider.steel2.SetActive(false);
+                key.SetActive(true);
+                isActive = false;
+            }
+        }
     }
 
     public void updateOven()
     {
         ovenLight1.intensity = lightIntense;
         ovenLight2.intensity = lightIntense;
-        if (collider.steelCount >= 2 && collider.mouldInside && door.transform.rotation.eulerAngles.y >= doorClosed)
-        {
-            collider.steel1.SetActive(false);
-            collider.steel2.SetActive(false);
-            key.SetActive(true);
-        }
+        isActive = true;
     }
 }
