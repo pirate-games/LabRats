4bc0ba2a0ccc90563d004221f030d231bb158bcd
Tiara Vrossink <71695366+arctantica@users.noreply.github.com>
2024-01-10 10:30:56 +0100

    door added

--- Lab Rats/Assets/Lab/Oven/Scripts/OvenFunctionality.cs
+++ Lab Rats/Assets/Lab/Oven/Scripts/OvenFunctionality.cs
@@ -9,11 +9,11 @@ public class OvenFunctionality : MonoBehaviour
     [SerializeField]
     private KeypadFunctionality keypad;
     [SerializeField]
-    private GameObject key;
+    private GameObject key, door;
     [SerializeField]
     private Light ovenLight1, ovenLight2;
 
-    private float lightIntense = 0.4f;
+    private float lightIntense = 0.4f, doorClosed = 88f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +24,12 @@ public class OvenFunctionality : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(door.transform.rotation.eulerAngles.y);
         if (keypad.correct)
         {
             ovenLight1.intensity = lightIntense;
             ovenLight2.intensity = lightIntense;
-            if(collider.steelCount >= 2 && collider.mouldInside)
+            if(collider.steelCount >= 2 && collider.mouldInside && door.transform.rotation.eulerAngles.y >= doorClosed)
             {
                 collider.steel1.SetActive(false);
                 collider.steel2.SetActive(false);
