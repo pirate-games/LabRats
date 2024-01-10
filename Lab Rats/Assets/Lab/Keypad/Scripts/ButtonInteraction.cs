1a6497cd8153ed5ff0fd4ad04a97dada8b17186e
Tiara Vrossink <71695366+arctantica@users.noreply.github.com>
2024-01-10 15:47:12 +0100

    button interaction ready for testing

---
+++ Lab Rats/Assets/Lab/Keypad/Scripts/ButtonInteraction.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ButtonInteraction : MonoBehaviour
+{
+    public Transform visualTarget;
+    public Vector3 localAxis;
+    public float resetSpeed = 5;
+    public float followAngleTreshold = 45;
+
+    private bool freeze;
+
+    private Vector3 initialLocalPos;
+
+    private Vector3 offset;
+    private Transform pokeAttachTransform;
+
+    private XRBaseInteractable interactible;
+    private bool isFollowing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialLocalPos = visualTarget.localPosition;
+
+        interactible = GetComponent<XRBaseInteractable>();
+        interactible.hoverEntered.AddListener(Follow);
+        interactible.hoverExited.AddListener(Reset);
+        interactible.selectEntered.AddListener(Freeze);
+    }
+
+    public void Follow(BaseInteractionEventArgs hover)
+    {
+        if(hover.interactorObject is XRPokeInteractor)
+        {
+            XRPokeInteractor interactor = (XRPokeInteractor)hover.interactorObject;
+
+            pokeAttachTransform = interactor.attachTransform;
+            offset = visualTarget.position - pokeAttachTransform.position;
+
+            float pokeAngle = Vector3.Angle(offset, visualTarget.TransformDirection(localAxis));
+
+            if(pokeAngle > followAngleTreshold)
+            {
+                isFollowing = true;
+                freeze = false;
+            }
+        }
+    }
+
+    public void Reset(BaseInteractionEventArgs hover)
+    {
+        if(hover.interactorObject is XRPokeInteractor)
+        {
+            isFollowing = false;
+            freeze = false;
+        }
+        
+    }
+
+    public void Freeze(BaseInteractionEventArgs hover)
+    {
+        if(hover.interactorObject is XRPokeInteractor)
+        {
+            freeze = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (freeze)
+            return;
+
+        if (isFollowing)
+        {
+            Vector3 localTargetPosition = visualTarget.InverseTransformPoint(pokeAttachTransform.position + offset);
+            Vector3 constrainedLocalTargetPosition = Vector3.Project(localTargetPosition, localAxis);
+            visualTarget.position = visualTarget.TransformPoint(constrainedLocalTargetPosition);
+        }
+        else
+        {
+            visualTarget.localPosition = Vector3.Lerp(visualTarget.localPosition, initialLocalPos, Time.deltaTime * resetSpeed);
+        }
+    }
+}
