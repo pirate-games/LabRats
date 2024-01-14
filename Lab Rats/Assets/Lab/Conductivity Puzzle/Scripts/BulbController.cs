bcc43fb4648e4fc14035881092e30db9979f53ba
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2024-01-14 16:36:14 +0100

    added smol fixes to conductivity

--- Lab Rats/Assets/Lab/Conductivity Puzzle/Scripts/BulbController.cs
+++ Lab Rats/Assets/Lab/Conductivity Puzzle/Scripts/BulbController.cs
@@ -1,48 +1,56 @@
-using System;
 using ElementsSystem;
 using UnityEngine;
 
-public class BulbController : MonoBehaviour
+namespace Lab.Conductivity_Puzzle.Scripts
 {
-    private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
-    
-    [Header("Materials")]
-    [SerializeField] private Material glassMaterial;
-    [SerializeField] private Material filamentMaterial;
-    
-    [Header("Settings")]
-    [SerializeField, Range(0, 10)] private float resistanceMultiplier = 5f;
-
-
-    private Color _originalGlassColor;
-    private Color _originalFilamentColor;
-
-    private void Start()
+    public class BulbController : MonoBehaviour
     {
-        _originalGlassColor = glassMaterial.GetColor(EmissionColor);
-        _originalFilamentColor = filamentMaterial.GetColor(EmissionColor);
-    }
+        private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
+    
+        [Header("Materials")]
+        [SerializeField] private Material glassMaterial;
+        [SerializeField] private Material filamentMaterial;
+    
+        [Header("Settings")]
+        [SerializeField, Range(0, 10)] private float resistanceMultiplier = 5f;
 
-    public ElementModel ElementModel
-    {
-        set
-        {            
-            var resistanceScaled = 1 - value.ElementObject.Resistivity * 10_000_000;
-            var resistance = 1 + Mathf.Clamp(resistanceScaled, 0f, 1f) * resistanceMultiplier * 10f;
-            
-            SetLightColor(glassMaterial,glassMaterial.GetColor(EmissionColor) * resistance);
-            SetLightColor(filamentMaterial,filamentMaterial.GetColor(EmissionColor) * resistance);
+
+        private Color _originalGlassColor;
+        private Color _originalFilamentColor;
+
+        private void Start()
+        {
+            _originalGlassColor = glassMaterial.GetColor(EmissionColor);
+            _originalFilamentColor = filamentMaterial.GetColor(EmissionColor);
         }
-    }
     
-    public void Reset()
-    {
-        SetLightColor(glassMaterial, _originalGlassColor);
-        SetLightColor(filamentMaterial, _originalFilamentColor);
-    }
+        private void OnDisable()
+        {
+            // reset colors when disabled to avoid issues with emission color
+            Reset();
+        }
+
+        public ElementModel ElementModel
+        {
+            set
+            {            
+                var resistanceScaled = 1 - value.ElementObject.Resistivity * 10_000_000;
+                var resistance = 1 + Mathf.Clamp(resistanceScaled, 0f, 1f) * resistanceMultiplier * 10f;
+            
+                SetLightColor(glassMaterial,glassMaterial.GetColor(EmissionColor) * resistance);
+                SetLightColor(filamentMaterial,filamentMaterial.GetColor(EmissionColor) * resistance);
+            }
+        }
     
-    private static void SetLightColor(Material material, Color color)
-    {
-        material.SetColor(EmissionColor, color);
+        public void Reset()
+        {
+            SetLightColor(glassMaterial, _originalGlassColor);
+            SetLightColor(filamentMaterial, _originalFilamentColor);
+        }
+    
+        private static void SetLightColor(Material material, Color color)
+        {
+            material.SetColor(EmissionColor, color);
+        }
     }
 }
