045ae9104e8a27ffb8cd01cad3e631e9e401fc4f
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2024-01-15 09:53:37 +0100

    final push before water

---
+++ Lab Rats/Assets/Lab/Reactivity Puzzle/WaterReactionManager.cs
@@ -0,0 +1,26 @@
+﻿using ElementsSystem;
+using UnityEngine;
+
+namespace Lab.Reactivity_Puzzle
+{
+    public class WaterReactionManager: MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem heavySmoke;
+        [SerializeField] private ParticleSystem lightSmoke;
+
+        public void ActivateReaction(ElementModel element)
+        {
+            if(element.ElementObject == null) return;
+            
+            switch (element.ElementObject.ElementType)
+            {
+                case ElementType.AlkaliMetal:
+                    heavySmoke.Play();
+                    break;
+                case ElementType.AlkalineEarthMetal:
+                    lightSmoke.Play();
+                    break;
+            }
+        }
+    }
+}
