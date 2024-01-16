c5b0c6cd580b62b4018101c9d1bcaf7b7f661e86
V Majarocon <56297035+Veerific@users.noreply.github.com>
2024-01-16 23:28:41 +0100

    Buttons are now click interactable

--- Lab Rats/Assets/Mulitplayer/ButtonController.cs
+++ Lab Rats/Assets/Mulitplayer/ButtonController.cs
@@ -8,16 +8,12 @@ using UnityEngine.Events;
 
 public class ButtonController : MonoBehaviour
 {
-    [SerializeField] private string playerTag;
-    [SerializeField] private UnityEvent OnButtonPress;
-    public bool hasBeenPressed {  get; private set; }
-    
-    private void OnCollisionEnter(Collision other)
+    [SerializeField] private UnityEvent onButtonPress;
+    public bool hasBeenPressed { get; private set; }
+
+    public void OnButtonPress()
     {
-        if (other.gameObject.CompareTag(playerTag) && !hasBeenPressed)
-        {
-            hasBeenPressed = true;
-            OnButtonPress.Invoke();
-        }
+        hasBeenPressed = true;
+        onButtonPress.Invoke();
     }
 }
