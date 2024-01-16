7c5cd34c6e1d13f0d3ce813021292eaaf933a783
V Majarocon <56297035+Veerific@users.noreply.github.com>
2024-01-17 00:14:48 +0100

    mend

--- Lab Rats/Assets/Mulitplayer/StartGame.cs
+++ Lab Rats/Assets/Mulitplayer/StartGame.cs
@@ -28,4 +28,31 @@ public class StartGame : MonoBehaviour
         doorLeft.transform.localRotation = _doorLTargetRotation;
         doorRight.transform.localRotation = _doorRTargetRotation;
     }
+    
+    //Remnants of me trying something a little more complex
+    //
+    // //Dictionary of the playerList, with the ulong presenting their Network clientId and the bool representing the ready state of the client
+    // private Dictionary<ulong, bool> _playerList = new();
+    // private bool _playerReady;
+    //
+    // public void CheckIfPlayerReady()
+    // {
+    //     var clientId = GetPlayerId();
+    //     if (_playerList[clientId]) return;
+    //     _playerList[clientId] = true;
+    // }
+    //
+    // [ServerRpc(RequireOwnership = false)]
+    // public void AddPlayerToList(ServerRpcParams serverRpcParams = default)
+    // {
+    //     var clientId = serverRpcParams.Receive.SenderClientId;
+    //     _playerList.Add(clientId, false);
+    //     Debug.Log(clientId);
+    // }
+    //
+    // [ServerRpc(RequireOwnership = false)]
+    // private ulong GetPlayerId(ServerRpcParams serverRpcParams = default)
+    // {
+    //     return serverRpcParams.Receive.SenderClientId;
+    // }
 }
