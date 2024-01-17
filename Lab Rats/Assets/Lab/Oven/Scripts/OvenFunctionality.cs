8b78c9581defe1a07a5fa7454779527072ab9926
Tiara Vrossink <71695366+arctantica@users.noreply.github.com>
2024-01-17 11:54:54 +0100

    cauldron movement added

--- Lab Rats/Assets/Lab/Oven/Scripts/OvenFunctionality.cs
+++ Lab Rats/Assets/Lab/Oven/Scripts/OvenFunctionality.cs
@@ -8,13 +8,17 @@ public class OvenFunctionality : NetworkBehaviour
     [SerializeField]
     private OvenCollider collider;
     [SerializeField]
-    private GameObject key, door;
+    private GameObject key, door, cauldron;
     [SerializeField]
     private ParticleSystem flames;
 
     private float doorClosed = 88f;
 
     private bool isActive;
+
+    private float pouringTime = 3;
+    private float timer = 0;
+    private bool poured;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +34,12 @@ public class OvenFunctionality : NetworkBehaviour
             if (collider.steelCount >= 2 && collider.mouldInside && door.transform.rotation.eulerAngles.y >= doorClosed)
             {
                 CreateKey();
+            }
+
+            if (poured)
+            {
+                key.SetActive(true);
+                isActive = false;
             }
         }
     }
@@ -78,7 +88,13 @@ public class OvenFunctionality : NetworkBehaviour
     {
         collider.steel1.SetActive(false);
         collider.steel2.SetActive(false);
-        key.SetActive(true);
-        isActive = false;
+        float t = timer / pouringTime;
+        cauldron.transform.rotation = Quaternion.Euler(Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(18, 0, 0), t));
+        timer += Time.deltaTime;
+        if (timer >= pouringTime)
+        {
+            poured = true;
+            timer = 0;
+        }
     }
 }
