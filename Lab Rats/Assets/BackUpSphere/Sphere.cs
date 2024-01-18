48307a5fc900bb27ea52c7b8c6bbf0fa0d7af988
Jordy Post <71704175+YoloJordy@users.noreply.github.com>
2024-01-18 13:11:04 +0100

    changed ownership logic

--- Lab Rats/Assets/BackUpSphere/Sphere.cs
+++ Lab Rats/Assets/BackUpSphere/Sphere.cs
@@ -29,27 +29,8 @@ namespace BackUpSphere
         // Call this method to initiate grabbing (e.g., when a VR hand touches the object)
         public void GrabObject()
         {
-            if (_thisNetworkObject.IsOwner)
-            {
-                Debug.Log("I'm the owner");
-                TryGiveServerRpc();
-            }
-            else
-            {
-                Debug.Log("Try grab");
-                TryGrabServerRpc();
-            }
-        }
-
-        // Example code to manually sync transform if needed
-        private void Update()
-        {
-            if (_thisNetworkObject.IsOwner)
-            {
-                // Perform actions that require ownership, e.g., syncing transform
-                // thisNetworkObject.transform.position = newPosition;
-                // thisNetworkObject.transform.rotation = newRotation;
-            }
+            Debug.Log("Try grab");
+            TryGrabServerRpc();
         }
     }
 }
