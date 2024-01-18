6f7d8515aa8f44bbfeeec9a6a71b302e3b7afcc5
Jordi van der Lem <43111967+jvdlem@users.noreply.github.com>
2024-01-18 15:16:05 +0100

    XRINtereactorMulti

--- Lab Rats/Assets/BackUpSphere/XRInteractorMultiplayer.cs
+++ Lab Rats/Assets/BackUpSphere/XRInteractorMultiplayer.cs
@@ -5,48 +5,61 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class XRInteractorMultiplayer : MonoBehaviour
 {
     [SerializeField] private XRBaseInteractor thisInteractor;
-    private NetworkObject thisNetworkObject;
 
     private void Start()
     {
-        //thisNetworkObject = GetComponent<NetworkObject>();
+        // No need to initialize thisNetworkObject here
     }
 
     [ServerRpc(RequireOwnership = false)]
     private void TryGrabServerRpc(ServerRpcParams serverRpcParams = default)
     {
         Debug.Log("TransferOwnership to " + serverRpcParams.Receive.SenderClientId);
-        thisNetworkObject.ChangeOwnership(serverRpcParams.Receive.SenderClientId);
+        // Use the correct object reference based on your XRInteraction system
+        // For example, if it's a XRGrabInteractable, you might need to replace it with the correct component type.
+        var interactableObject = thisInteractor.selectTarget.GetComponent<NetworkObject>();
+        if (interactableObject != null)
+        {
+            thisInteractor.selectTarget.transform.parent = null;
+            interactableObject.ChangeOwnership(serverRpcParams.Receive.SenderClientId);
+        }
     }
 
     [ServerRpc(RequireOwnership = false)]
     private void TryGiveServerRpc()
     {
         Debug.Log("removing ownership");
-        thisNetworkObject.RemoveOwnership();
+        // Use the correct object reference based on your XRInteraction system
+        // For example, if it's a XRGrabInteractable, you might need to replace it with the correct component type.
+        var interactableObject = thisInteractor.selectTarget.GetComponent<NetworkObject>();
+        if (interactableObject != null)
+        {
+            thisInteractor.selectTarget.transform.parent = null;
+            interactableObject.RemoveOwnership();
+        }
     }
 
     // Call this method to initiate grabbing (e.g., when a VR hand touches the object)
     public void GrabObject()
     {
-        if (thisNetworkObject.IsOwner)
+        var interactableObject = thisInteractor.selectTarget.GetComponent<NetworkObject>();
+        if (interactableObject != null)
         {
-            Debug.Log("I'm the owner");
-            TryGiveServerRpc();
-        }
-        else
-        {
-            Debug.Log("Try grab");
-            TryGrabServerRpc();
+            if (interactableObject.IsOwner)
+            {
+                Debug.Log("I'm the owner");
+                TryGiveServerRpc();
+            }
+            else
+            {
+                Debug.Log("Try grab");
+                TryGrabServerRpc();
+            }
         }
     }
 
     public void OnSelectGrabbable(SelectEnterEventArgs eventArgs)
-    {    
-        thisNetworkObject = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
-        if (thisNetworkObject != null)
-        {
-            GrabObject();
-        }
+    {
+        GrabObject();
     }
 }
