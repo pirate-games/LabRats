44dce6cc93997ebda24ffd8e9efd1dcba9354ac8
Jordy Post <71704175+YoloJordy@users.noreply.github.com>
2024-01-19 13:40:50 +0100

    reverted to settings from main

--- Lab Rats/Assets/BackUpSphere/Sphere.cs
+++ Lab Rats/Assets/BackUpSphere/Sphere.cs
@@ -5,10 +5,53 @@ using NetworkPlayer = Player.Scripts.NetworkPlayer;
 
 namespace BackUpSphere
 {
+    //public class Sphere : NetworkBehaviour
+    //{
+    //    private NetworkObject _thisNetworkObject;
+    //
+    //
+    //    private void Start()
+    //    {
+    //        _thisNetworkObject = GetComponent<NetworkObject>();
+    //    }
+    //
+    //    [ServerRpc(RequireOwnership = false)]
+    //    private void TryGrabServerRpc(ServerRpcParams serverRpcParams = default)
+    //    {
+    //        var clientId = serverRpcParams.Receive.SenderClientId;
+    //        var player = NetworkPlayer.Players[clientId].NetworkObject;
+    //
+    //        if (player == null) return;
+    //
+    //        Debug.Log("TransferOwnership to " + clientId);
+    //        _thisNetworkObject.ChangeOwnership(clientId);
+    //    }
+    //
+    //    [ServerRpc(RequireOwnership = false)]
+    //    private void TryReleaseServerRpc()
+    //    {
+    //        Debug.Log("removing ownership");
+    //
+    //        _thisNetworkObject.RemoveOwnership();
+    //    }
+    //
+    //    // Call this method to initiate grabbing (e.g., when a VR hand touches the object)
+    //    public void GrabObject()
+    //    {
+    //        Debug.Log("Try grab");
+    //        TryGrabServerRpc();
+    //    }
+    //
+    //    public void ReleaseObject()
+    //    {
+    //        Debug.Log("Releasing Object");
+    //        TryReleaseServerRpc();
+    //    }
+    //}
+
     public class Sphere : NetworkBehaviour
     {
         private NetworkObject _thisNetworkObject;
-
 
         private void Start()
         {
@@ -18,34 +61,30 @@ namespace BackUpSphere
         [ServerRpc(RequireOwnership = false)]
         private void TryGrabServerRpc(ServerRpcParams serverRpcParams = default)
         {
-            var clientId = serverRpcParams.Receive.SenderClientId;
-            var player = NetworkPlayer.Players[clientId].NetworkObject;
-
-            if (player == null) return;
-
-            Debug.Log("TransferOwnership to " + clientId);
-            _thisNetworkObject.ChangeOwnership(clientId);
+            Debug.Log("TransferOwnership to " + serverRpcParams.Receive.SenderClientId);
+            _thisNetworkObject.ChangeOwnership(serverRpcParams.Receive.SenderClientId);
         }
 
         [ServerRpc(RequireOwnership = false)]
-        private void TryReleaseServerRpc()
+        private void TryGiveServerRpc()
         {
             Debug.Log("removing ownership");
-
             _thisNetworkObject.RemoveOwnership();
         }
 
         // Call this method to initiate grabbing (e.g., when a VR hand touches the object)
         public void GrabObject()
         {
-            Debug.Log("Try grab");
-            TryGrabServerRpc();
-        }
-
-        public void ReleaseObject()
-        {
-            Debug.Log("Releasing Object");
-            TryReleaseServerRpc();
+            if (_thisNetworkObject.IsOwner)
+            {
+                //Debug.Log("I'm the owner");
+                //TryGiveServerRpc();
+            }
+            else
+            {
+                Debug.Log("Try grab");
+                TryGrabServerRpc();
+            }
         }
     }
 }
