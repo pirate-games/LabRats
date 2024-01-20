4a949658d6e6d9f5cf46ffff009340afc0c799bf
Jordy Post <71704175+YoloJordy@users.noreply.github.com>
2024-01-20 14:41:58 +0100

    Wrote simple socket

---
+++ Lab Rats/Assets/ElementSocket.cs
@@ -0,0 +1,27 @@
+using ElementsSystem;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ElementSocket : MonoBehaviour
+{
+    [SerializeField]
+    Transform attachTransform;
+
+    private void Start()
+    {
+        if (attachTransform == null)
+        {
+            attachTransform = transform;
+        }
+    }
+
+    public void Enter(ElementModel obj)
+    {
+        obj.transform.SetPositionAndRotation(attachTransform.position, attachTransform.rotation);
+        if (!obj.TryGetComponent<XRGrabInteractable>(out var m_Interactable) || m_Interactable.firstInteractorSelecting == null)
+        {
+            return;
+        }
+        m_Interactable.interactionManager.SelectExit(m_Interactable.firstInteractorSelecting, m_Interactable);
+    }
+}
