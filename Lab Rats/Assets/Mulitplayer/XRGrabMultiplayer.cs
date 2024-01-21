2d08a6ded405bae749498a85654835c1c91e566b
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2024-01-21 13:07:14 +0100

    tried to fix kinematic issue on grab

---
+++ Lab Rats/Assets/Mulitplayer/XRGrabMultiplayer.cs
@@ -0,0 +1,57 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Mulitplayer
+{
+    public class XRGrabMultiplayer : NetworkBehaviour
+    {
+        private NetworkObject _thisNetworkObject;
+        private Rigidbody _rigidbody;
+
+        private void Start()
+        {
+            _thisNetworkObject = GetComponent<NetworkObject>();
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void TryGrabServerRpc(ServerRpcParams serverRpcParams = default)
+        {
+            if (!_thisNetworkObject) return;
+            
+            _thisNetworkObject.ChangeOwnership(serverRpcParams.Receive.SenderClientId);
+            
+            if (!_rigidbody) return;
+
+            _rigidbody.isKinematic = false;
+        }
+        
+        [ServerRpc(RequireOwnership = false)]
+        private void TryReleaseServerRpc()
+        {
+            if (!_thisNetworkObject) return;
+            
+            _thisNetworkObject.RemoveOwnership();
+            
+            if (!_rigidbody) return;
+
+            _rigidbody.isKinematic = true;
+        }
+
+        // Call this method to initiate grabbing (e.g., when a VR hand touches the object)
+        public void GrabObject()
+        {
+            if (_thisNetworkObject.IsOwner) return;
+         
+            TryGrabServerRpc();
+        }
+        
+        //Stop grabbing (e.g., when a VR hand stops touching the object)
+        public void ReleaseObject()
+        {
+            if (_thisNetworkObject.IsOwner) return;
+         
+            TryReleaseServerRpc();
+        }
+    }
+}
