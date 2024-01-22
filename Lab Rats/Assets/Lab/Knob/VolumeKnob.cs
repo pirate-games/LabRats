e59ccf12145a3a245719aeaba3d97b678e4bda8c
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2024-01-22 09:10:54 +0100

    added knob to radio (not functional yet) + ps for music

---
+++ Lab Rats/Assets/Lab/Knob/VolumeKnob.cs
@@ -0,0 +1,12 @@
+﻿using Unity.VRTemplate;
+using UnityEngine;
+
+namespace Lab.Knob
+{
+    public class VolumeKnob: XRKnob
+    {
+        [SerializeField] private AudioSource audioSource;
+        
+        private void FixedUpdate() => audioSource.volume = value;
+    }
+}
