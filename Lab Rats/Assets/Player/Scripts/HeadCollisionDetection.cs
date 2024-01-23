25e379ecbfbf2f15e431a9a74de902a958d3bc75
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2024-01-23 11:05:44 +0100

    head collision

---
+++ Lab Rats/Assets/Player/Scripts/HeadCollisionDetection.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.Scripts
+{
+    public class HeadCollisionDetection : MonoBehaviour
+    {
+        [Header("Detection Settings")] [SerializeField, Range(0, 0.5f)]
+        private float detectionDelay = 0.05f;
+
+        [SerializeField] private float detectionDistance = 0.2f;
+
+        [Header("Detection Layers")] [SerializeField]
+        private LayerMask detectionLayers;
+
+        private float _detectionTimer;
+
+        public List<RaycastHit> Hits { get; set; } = new();
+
+        private void Start()
+        {
+            Hits = Detection(transform.position, detectionDistance, detectionLayers);
+        }
+
+        private void FixedUpdate()
+        {
+            _detectionTimer += Time.deltaTime;
+
+            if (!(_detectionTimer > detectionDelay)) return;
+
+            Hits = Detection(transform.position, detectionDistance, detectionLayers);
+            _detectionTimer = 0;
+        }
+
+        /// <summary>
+        ///   Detects collisions with the head
+        /// </summary>
+        /// <param name="pos"> the position of the collision </param>
+        /// <param name="dist"> the direction of the collision </param>
+        /// <param name="layerMask"> the layer mask used for the collisions </param>
+        /// <returns></returns>
+        private List<RaycastHit> Detection(Vector3 pos, float dist, LayerMask layerMask)
+        {
+            var detectedHits = new List<RaycastHit>();
+
+            var directions = new List<Vector3>
+            {
+                // directions that will be checked
+                transform.forward,
+                transform.right,
+                -transform.right,
+                transform.up
+            };
+
+            foreach (var direction in directions)
+            {
+                if (Physics.Raycast(pos, direction, out var hit, dist, layerMask))
+                {
+                    detectedHits.Add(hit);
+                }
+            }
+
+            return detectedHits;
+        }
+    }
+}
---
+++ Lab Rats/Assets/Player/Scripts/HeadCollisionManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Player.Scripts
+{
+    public class HeadCollisionManager : MonoBehaviour
+    {
+        [SerializeField] private HeadCollisionDetection headCollisionDetection;
+        [SerializeField] private CharacterController characterController;
+        [SerializeField] private float pushBackForce = 1.0f;
+        
+        private static Vector3 CalculatePushBackDirection(IEnumerable<RaycastHit> hits)
+        {
+            return hits.Aggregate(Vector3.zero, (current, hit) => current + new Vector3(current.x, 0, current.z));
+        }
+        
+        private void FixedUpdate()
+        {
+            if (headCollisionDetection.Hits.Count <= 0) return;
+            
+            var pushBackDirection = CalculatePushBackDirection(headCollisionDetection.Hits);
+            characterController.Move(pushBackDirection.normalized * (pushBackForce * Time.deltaTime));
+        }
+    }
+}
