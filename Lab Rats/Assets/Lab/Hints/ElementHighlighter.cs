d1335d626d3bbfe26aa18cb71c5115200061eefe
V Majarocon <56297035+Veerific@users.noreply.github.com>
2024-01-29 16:17:34 +0100

    Added element highlighter to main scene

--- Lab Rats/Assets/Lab/Hints/ElementHighlighter.cs
+++ Lab Rats/Assets/Lab/Hints/ElementHighlighter.cs
@@ -8,14 +8,14 @@ namespace Lab.Hints
     public class ElementHighlighter : MonoBehaviour
     {
         [SerializeField] private GameObject positionList;
-        private Dictionary<string, Vector3> _elementDict = new();
+        private readonly Dictionary<int, Vector3> _elementDict = new();
         [SerializeField] private GameObject highlighter;
 
         private void Start()
         {
             foreach (Transform child in positionList.transform)
             {
-                var elementNumber = child.name;
+                var elementNumber = int.Parse(child.name);
                 var elementPosition = child.position;
                 _elementDict.Add(elementNumber, elementPosition);
             }
@@ -23,7 +23,7 @@ namespace Lab.Hints
 
         public void HighlightElement(int num)
         {
-            var element = _elementDict[num.ToString()];
+            var element = _elementDict[num];
             highlighter.transform.position = new(element.x, element.y, 
                 highlighter.transform.position.z);
         }
